ae41cd13b0cc97d09b962b178e30aa6c1d4e779b
Inrixia <Inrixia@gmail.com>
2023-08-20 08:26:59 +1200

    Small optimizations

--- plugins/TidalTags/src/index.js
+++ plugins/TidalTags/src/index.js
@@ -69,26 +69,30 @@ const updateTrackElements = (trackElements) => {
 
 const streamQualitySelector = "data-test-media-state-indicator-streaming-quality";
 
-const unloadIntercept = intercept(`playbackControls/SET_PLAYBACK_STATE`, () => {
+const unloadIntercept = intercept(["playbackControls/SET_PLAYBACK_STATE"], () => {
 	const streamQuality = document.querySelector(`[${streamQualitySelector}]`);
 	const currentQuality = streamQuality.getAttribute(streamQualitySelector);
 	const qualityElement = streamQuality.children[0];
-	qualityElement.style.backgroundColor = null;
-	qualityElement.style.color = null;
 	if (qualityElement === null) return;
 	switch (currentQuality) {
 		// MQA
 		case "HI_RES":
 			if (qualityElement.textContent === "MQA") return;
 			qualityElement.textContent = "MQA";
+
 			qualityElement.style.backgroundColor = null;
 			qualityElement.style.color = tagData[Quality.MQA].color;
 			break;
 		case "HI_RES_LOSSLESS":
 			if (qualityElement.textContent === "HIRES") return;
 			qualityElement.textContent = "HI-RES";
+
+			qualityElement.style.backgroundColor = null;
 			qualityElement.style.color = tagData[Quality.HiRes].color;
 			break;
+		default:
+			qualityElement.style.backgroundColor = null;
+			qualityElement.style.color = null;
 	}
 });
 
