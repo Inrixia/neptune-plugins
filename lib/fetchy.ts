b22465668e87d1813019a6c70d2f52ef63f5fc2a
Inrixia <Inrixia@gmail.com>
2024-02-11 19:33:35 +1300

    Use correct value for x-tidal-token header

--- lib/fetchy.ts
+++ lib/fetchy.ts
@@ -7,7 +7,7 @@ export const getHeaders = (): Record<string, string> => {
 	const state = store.getState();
 	return {
 		Authorization: `Bearer ${state.session.oAuthAccessToken}`,
-		"x-tidal-token": <string>state.session.oAuthAccessToken,
+		"x-tidal-token": <string>(<any>state.session.clientId),
 	};
 };
 
