bf78025f172ebdb6afa1b368498041864f5c98ba
Inrixia <Inrixia@gmail.com>
2024-06-14 16:48:58 +1200

    SongDownloader - Add Black Magic aka node side fs saving to use predefined paths

--- plugins/SongDownloader/src/Settings.ts
+++ plugins/SongDownloader/src/Settings.ts
@@ -5,10 +5,15 @@ import { storage } from "@plugin";
 import { AudioQualityInverse, AudioQualityEnum, validQualitiesSettings } from "../../../lib/AudioQuality";
 
 storage.desiredDownloadQuality ??= AudioQualityEnum.HiRes;
+storage.defaultDownloadPath ??= "";
 export const Settings = () => html`<div class="settings-section">
 	<h3 class="settings-header">Download Quality</h3>
 	<p class="settings-explainer">Select the desired max download quality:</p>
 	<select id="qualityDropdown" onChange=${(event: { target: { value: unknown } }) => (storage.desiredDownloadQuality = event.target.value)}>
 		${validQualitiesSettings.map((quality) => html`<option value=${quality} selected=${storage.desiredDownloadQuality === quality}>${AudioQualityInverse[quality]}</option>`)}
 	</select>
+
+	<h3 class="settings-header">Download Path</h3>
+	<p class="settings-explainer">System path to save to. Doing so will disable download prompt</p>
+	<input onChange=${({ target }: { target: { value: string } }) => (storage.defaultDownloadPath = target.value)} value=${storage.defaultDownloadPath} />
 </div>`;
--- plugins/SongDownloader/src/saveFile.ts
+++ plugins/SongDownloader/src/saveFile.ts
@@ -1,4 +1,13 @@
-export const saveFile = (blob: Blob, fileName: string) => {
+import type * as fs from "fs/promises";
+const { writeFile } = <typeof fs>require("fs/promises");
+
+// @ts-expect-error Remove this when types are available
+import { storage } from "@plugin";
+
+export const saveFile = async (blob: Blob, fileName: string) => {
+	if (storage.defaultDownloadPath !== undefined) {
+		return await writeFile(storage.defaultDownloadPath + "/" + fileName, Buffer.from(await blob.arrayBuffer()));
+	}
 	// Create a new Object URL for the Blob
 	const objectUrl = URL.createObjectURL(blob);
 
