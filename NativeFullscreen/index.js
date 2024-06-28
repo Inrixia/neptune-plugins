a48bd5149504a0243e4e61a01c00fe1c637f95ec
Inrixia <Inrixia@users.noreply.github.com>
2024-06-28 13:34:53 +0000

    deploy: 3e94f11a1d0e07c71014120fdacffb2fc88af92a

--- NativeFullscreen/index.js
+++ NativeFullscreen/index.js
@@ -1,9 +1,11 @@
-import{intercept as c}from"@neptune";import{html as a}from"@neptune/voby";import{storage as s}from"@plugin";var i=e=>{s.settings??={};for(let t of Object.keys(e))s.settings[t]??=e[t];return s.settings};import{html as d}from"@neptune/voby";var l=({checked:e,onClick:t,title:n})=>(e??=!1,d`
-		<div style="margin-bottom: 15px;display: flex;justify-content: space-between;">
+import{intercept as d}from"@neptune";import{html as g}from"@neptune/voby";import{storage as i}from"@plugin";var r=e=>{i.settings??={};for(let t of Object.keys(e))i.settings[t]??=e[t];return i.settings};import{html as p}from"@neptune/voby";import{html as a}from"@neptune/voby";var l=({children:e,tooltip:t})=>a`
+	<div style="margin-bottom: 15px;display: flex;justify-content: space-between;align-items: center;" title="${t}">${e}</div>
+`;var c=({checked:e,onClick:t,title:n,tooltip:m})=>(e??=!1,p`
+		<${l} tooltip=${m}>
 			<label for="switch-${n}" style="font-size: 1.2em;margin-bottom: 5px;">${n}</label>
 			<input id="switch-${n}" class="neptune-switch-checkbox" type="checkbox" checked=${e} />
 			<span onClick=${t} class="neptune-switch" />
-		</div>
-	`);var o=i({useTidalFullscreen:!1}),m=()=>a`<div>
-	<${l} checked=${o.useTidalFullscreen} onClick=${()=>o.useTidalFullscreen=!o.useTidalFullscreen} title="Always use Tidal Fullscreen mode" />
-</div>`;var r,p=c("view/FULLSCREEN_ALLOWED",()=>r||o.useTidalFullscreen?r=void 0:!0),g=c("view/REQUEST_FULLSCREEN",()=>{r=!0}),u=e=>{if(e.key==="F11"&&(e.preventDefault(),document.fullscreenElement?document.exitFullscreen():document.documentElement.requestFullscreen(),!o.useTidalFullscreen&&!document.fullscreenElement)){let t=document.querySelector("div[class^='bar--']"),n=document.querySelector("div[class^='mainContainer--'] > div[class^='containerRow--']");t!==null&&n!==null&&(document.fullscreenElement?(n.style.maxHeight="",t.style.display="",document.body.removeAttribute("is-fullscreen")):(n.style.maxHeight="100%",t.style.display="none",document.body.setAttribute("is-fullscreen","")))}};window.addEventListener("keydown",u);var $=()=>{p(),g(),window.removeEventListener("keydown",u)};export{m as Settings,$ as onUnload};
+		<//>
+	`);var o=r({useTidalFullscreen:!1}),y=()=>g`<div>
+	<${c} checked=${o.useTidalFullscreen} onClick=${()=>o.useTidalFullscreen=!o.useTidalFullscreen} title="Always use Tidal Fullscreen mode" />
+</div>`;var s,f=d("view/FULLSCREEN_ALLOWED",()=>s||o.useTidalFullscreen?s=void 0:!0),S=d("view/REQUEST_FULLSCREEN",()=>{s=!0}),u=e=>{if(e.key==="F11"&&(e.preventDefault(),document.fullscreenElement?document.exitFullscreen():document.documentElement.requestFullscreen(),!o.useTidalFullscreen&&!document.fullscreenElement)){let t=document.querySelector("div[class^='bar--']"),n=document.querySelector("div[class^='mainContainer--'] > div[class^='containerRow--']");t!==null&&n!==null&&(document.fullscreenElement?(n.style.maxHeight="",t.style.display="",document.body.removeAttribute("is-fullscreen")):(n.style.maxHeight="100%",t.style.display="none",document.body.setAttribute("is-fullscreen","")))}};window.addEventListener("keydown",u);var q=()=>{f(),S(),window.removeEventListener("keydown",u)};export{y as Settings,q as onUnload};
