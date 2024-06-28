d716161aa47314e53f2c825912fa5b240e3fb03e
Inrixia <Inrixia@gmail.com>
2024-06-28 16:30:29 +1200

    lib - Even better retry handling

--- plugins/_lib/nativeBridge/native/request/requestStream.native.ts
+++ plugins/_lib/nativeBridge/native/request/requestStream.native.ts
@@ -7,32 +7,31 @@ let defaultUserAgent: string | undefined = undefined;
 export const setDefaultUserAgent = (userAgent: string) => (defaultUserAgent = userAgent);
 
 // Cap to two requests per domain at a time
-const activeDomains: Record<string, Semaphore> = {};
-export type ExtendedRequestOptions = RequestOptions & { body?: string };
+const rateLimitSema = new Semaphore(1);
+export type ExtendedRequestOptions = RequestOptions & { body?: string; rateLimit?: number };
 export const requestStream = async (url: string, options: ExtendedRequestOptions = {}): Promise<IncomingMessage> => {
-	const domain = url.split("/")[2];
-	activeDomains[domain] ??= new Semaphore(4);
-	const release = await activeDomains[domain].obtain();
+	options.headers ??= {};
+	options.headers["user-agent"] = defaultUserAgent;
+	options.rateLimit ??= 0;
+	const release = options.rateLimit > 0 ? await rateLimitSema.obtain() : undefined;
 	return new Promise<IncomingMessage>((resolve, reject) => {
-		const body = options.body;
-		delete options.body;
-		options.headers ??= {};
-		options.headers["user-agent"] = defaultUserAgent;
 		const req = request(url, options, (res) => {
 			const statusMsg = res.statusMessage !== "" ? ` - ${res.statusMessage}` : "";
 			if (res.statusCode === 429 || res.statusCode === 503) {
-				const retryAfter = parseInt(res.headers["retry-after"] ?? (Math.floor(Math.random() * 10) + 5).toString(), 10);
-				libTrace.debug(`[${res.statusCode}${statusMsg}] (${req.method}) - Retrying in ${retryAfter}s`, url);
+				const retryAfter = parseInt(res.headers["retry-after"] ?? "1", 10);
+				options.rateLimit!++;
+				libTrace.debug(`[${res.statusCode}${statusMsg}] (${req.method})`, `[Attempt ${options.rateLimit}, Retry in ${retryAfter}s]`, url);
 				return setTimeout(() => {
-					release();
+					release?.();
 					requestStream(url, options).then(resolve, reject);
 				}, retryAfter);
 			}
-			libTrace.debug(`[${res.statusCode}${statusMsg}] (${req.method})`, url);
+			if (options.rateLimit! > 0) libTrace.debug(`[${res.statusCode}${statusMsg}] (${req.method})`, `[After ${options.rateLimit} attempts]`, url);
+			else libTrace.debug(`[${res.statusCode}${statusMsg}] (${req.method})`, url);
 			resolve(res);
 		});
-
 		req.on("error", reject);
+		const body = options.body;
 		if (body !== undefined) req.write(body);
 		req.end();
 	}).finally(release);
