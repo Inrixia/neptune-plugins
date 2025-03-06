06d0e68d256977f2ca7832a2b86bf99e5c009705
Inrixia <Inrixia@gmail.com>
2025-03-06 14:30:32 +1300

    Update findModuleFuction for latest Neptune/Tidal update

--- plugins/_lib/findModuleFunction.ts
+++ plugins/_lib/findModuleFunction.ts
@@ -1,11 +1,24 @@
-import { modules } from "@neptune";
+// @ts-expect-error modules has changed to moduleCache
+import { moduleCache } from "@neptune";
+import type { UnknownRecord } from "@inrixia/helpers/ts";
 
-export const findModuleFunction = <T>(propertyName: string, propertyType: string): T | undefined => {
-	for (const module of modules) {
-		if (typeof module?.exports !== "object") continue;
-		for (const _key in module.exports) {
-			const property = module.exports[_key]?.[propertyName];
-			if (typeof property === propertyType) return property;
+export const findModuleFunction = <T>(propertyName: string, propertyType: string): T | undefined => recursiveSearch<T>(moduleCache, propertyName, propertyType);
+
+const recursiveSearch = <T>(obj: UnknownRecord, propertyName: string, propertyType: string, seen = new Set<UnknownRecord>()): T | undefined => {
+	if (seen.has(obj)) return;
+	seen.add(obj);
+	for (const key of getKeys(obj)) {
+		const prop = obj[key];
+		if (typeof prop === "object" && prop !== null) {
+			const found = recursiveSearch<T>(<UnknownRecord>prop, propertyName, propertyType, seen);
+			if (found !== undefined) return found;
 		}
+		if (key === propertyName && typeof prop === propertyType) return <T>prop;
 	}
+};
+
+const getKeys = (obj: UnknownRecord) => {
+	const keys = Object.keys(obj);
+	if (keys.length !== 0) return keys;
+	return Reflect.ownKeys(Object.getPrototypeOf(obj) ?? obj);
 };
