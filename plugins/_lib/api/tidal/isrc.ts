33a9068c99f482789a226449e7a1c8e6676917bc
Inrixia <Inrixia@gmail.com>
2025-03-06 21:37:58 +1300

    TidalApi/RealMAX - Add additional error handling
    Fixes #121, #119, #120

--- plugins/_lib/api/tidal/isrc.ts
+++ plugins/_lib/api/tidal/isrc.ts
@@ -1,6 +1,7 @@
 import { TApiTrack, TApiTracks } from "./types";
 import { getToken } from "./auth";
 import { requestJsonCached } from "../../native/request/requestJsonCached";
+import { libTrace } from "../../trace";
 
 const fetchTidal = async <T>(url: string) =>
 	requestJsonCached<T>(url, {
@@ -13,11 +14,11 @@ const fetchTidal = async <T>(url: string) =>
 const baseURL = "https://openapi.tidal.com/v2";
 
 export async function* fetchIsrcIterable(isrc: string): AsyncIterable<TApiTrack> {
-	let resp = await fetchTidal<TApiTracks>(`${baseURL}/tracks?countryCode=US&filter[isrc]=${isrc}`);
+	let resp = await fetchTidal<TApiTracks | undefined>(`${baseURL}/tracks?countryCode=US&filter[isrc]=${isrc}`);
 	while (true) {
 		if (resp?.data === undefined || resp.data.length === 0) break;
 		yield* resp.data;
 		if (resp.links.next === undefined) break;
-		resp = await fetchTidal<TApiTracks>(`${baseURL}${resp.links.next}`);
+		resp = await fetchTidal<TApiTracks>(`${baseURL}${resp.links.next}`).catch((err) => libTrace.err(`Unexpected error when fetching Tidal ISRC ${isrc}: ${err}`));
 	}
 }
