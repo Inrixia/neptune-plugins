c7f7c4973e8b891fcb07542808babe1fd5218a54
Inrixia <Inrixia@gmail.com>
2025-04-03 10:23:27 +1300

    Fix #137

--- plugins/_lib/findModuleFunction.ts
+++ plugins/_lib/findModuleFunction.ts
@@ -4,16 +4,21 @@ import type { UnknownRecord } from "@inrixia/helpers/ts";
 
 export const findModuleFunction = <T>(propertyName: string, propertyType: string): T | undefined => recursiveSearch<T>(moduleCache, propertyName, propertyType);
 
+// @ts-expect-error
+window.findModuleFunction = findModuleFunction;
+
 const recursiveSearch = <T>(obj: UnknownRecord, propertyName: string, propertyType: string, seen = new Set<UnknownRecord>()): T | undefined => {
 	if (seen.has(obj)) return;
 	seen.add(obj);
 	for (const key of getKeys(obj)) {
-		const prop = obj[key];
-		if (typeof prop === "object" && prop !== null) {
-			const found = recursiveSearch<T>(<UnknownRecord>prop, propertyName, propertyType, seen);
-			if (found !== undefined) return found;
-		}
-		if (key === propertyName && typeof prop === propertyType) return <T>prop;
+		try {
+			const prop = obj[key];
+			if (typeof prop === "object" && prop !== null) {
+				const found = recursiveSearch<T>(<UnknownRecord>prop, propertyName, propertyType, seen);
+				if (found !== undefined) return found;
+			}
+			if (key === propertyName && typeof prop === propertyType) return <T>prop;
+		} catch {}
 	}
 };
 
